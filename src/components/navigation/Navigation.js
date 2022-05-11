3a40026a2e9b82f502ade2b5cf75696d3e2972cf
David Gurule <davidgurulejr@gmail.com>
2022-05-11 08:32:31 -0600

    adds navigation to the about page

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,7 +1,19 @@
 import './Navigation.css'
+import Resume from '../../assets/docs/david_gurule_resume_050422.pdf'
+
+const linkStyle = {
+  textDecoration: "none",
+  color: "white"
+}
 
 export default function Navigation() {
   return (
-    <div>Navigation</div>
+    <div className="Nav">
+     <ul className="navItems">
+       <li>PROJECTS</li>
+       <li><a className="resume" href={Resume} target="_blank" style={linkStyle}>RESUME</a></li>
+      <li><a href="mailto:davidgurulejr@gmail.com?subject=Nice to meet you Dave..." style={linkStyle}>CONTACT</a></li>
+     </ul>
+    </div>
   )
 }
