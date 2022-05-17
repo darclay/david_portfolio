a453ecf0e08567d7850a65a1adc29a57ceb3123b
David Gurule <davidgurulejr@gmail.com>
2022-05-17 10:47:13 -0600

    adds another layer to the paralax

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,11 +1,9 @@
 import './Home.css'
-// import Banner from '../../components/banner/Banner'
 import About from '../../components/about/About'
-// import heroPhoto from '../../assets/pics/main_photo.jpg'
 import Projects from '../../components/projects/Projects'
 import background from '../../assets/pics/background.png'
-import middleground from '../../assets/pics/middleground.png'
 import foreground from '../../assets/pics/foreground.png'
+import frontground from '../../assets/pics/frontground.png'
 
 export default function Home() {
   return (
@@ -13,8 +11,8 @@ export default function Home() {
       <div className="scrollContainer">
         <header>  
           <img className="background" src={background}/>
-          {/* <img className="middleground" src={middleground}/> */}
           <img className="foreground" src={foreground}/>
+          <img className="frontground" src={frontground}/>
         </header>
         <section>
           <About/>
