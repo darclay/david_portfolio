40d215f960bd27ae2d759f138ad121c8b4d6881a
David Gurule <davidgurulejr@gmail.com>
2022-05-23 16:34:01 -0600

    removes the js function to hover and replaces it with a hover feature for projects with css

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,7 +8,7 @@ export default function About() {
 
       <div className="aboutContainer">
         <h1 className="intro">Hello<br/>I'm Dave</h1>
-        <p className="aboutContent">Mountain Biker, Sender, Coder, Peopler?...are just a few words that represent me.  Hello again and nice to meet you.  I am a people person who spent the last 17 years in law enforcement. I take that same passion for helping others out on the streets and now code to help people virtually.  Outside of developing I enjoy being on an adventure with my wife, two boys, and two dogs (Bailey & Sage)  Check out some of my work below and don't hesitate to say hi.</p>
+        <p className="aboutContent fade-in">Mountain Biker, Sender, Coder, Peopler?...are just a few words that represent me.  Hello again and nice to meet you.  I am a people person who spent the last 17 years in law enforcement. I take that same passion for helping others out on the streets and now code to help people virtually.  Outside of developing I enjoy being on an adventure with my wife, two boys, and two dogs (Bailey & Sage)  Check out some of my work below and don't hesitate to say hi.</p>
       </div>
     </div>
   )
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,24 +6,14 @@ import Mobile from '../../assets/pics/smartphone.png'
 
 export default function Projects({project, index}) {
 
-  const [hiddenStatus, setHiddenStatus] = useState('hidden')
-
-//THESE FUNCTION SET THE MODULE TO SHOW OR HIDE ON HOVER.
-  function handleShow(ev){
-    setHiddenStatus('')
-  }
-  function handleHidden(){
-    setHiddenStatus('hidden')
-  }
-
    return (
     <div className="ProjectsDiv">
         <div 
-          className="projectGroup"
-          onMouseOver={(ev)=>{handleShow(ev)}}
-          onMouseOut={(ev)=>{handleHidden(ev)}}
-        >
-          <div key={index} className={`${hiddenStatus} hoverInfo`}>
+          className="projectGroup">
+          <div 
+            key={index} 
+            className="showMore hoverInfo"
+          >
             <p id={index} className="projectDescription">{project.miscInfo}</p>
           </div> 
 
