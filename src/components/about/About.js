765c86e19cff74e7d9dd55c7991010534b12fb71
David Gurule <davidgurulejr@gmail.com>
2022-07-11 17:42:58 -0600

    adds the hook 'react-intersection-observer' and creates animations on the intro and about section.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,14 +1,24 @@
+import {useInView} from 'react-intersection-observer'
 import './About.css'
 import Navigation from '../navigation/Navigation'
 
 export default function About() {
+// below instead of using useEffect, useRef, and useState I used "npm install react-intersection-observer --save" and then imported at the top "import {useInView} from 'react-intersection-observer'".
+//This allows me to use: "useInView()" hook.  I am duplicating them below and giving "ref" and "inView" specific names to use in the html below.  This creates a boolean when the ref becomes visible.
+
+  // const {ref: aboutRef, inView: aboutIsVisible} = useInView()
+  // const {ref: introRef, inView: introIsVisible} = useInView()  
+  const [aboutRef, aboutIsVisible] = useInView({threshold: .75})
+  const [introRef, introIsVisible] = useInView({threshold: .75}) 
+
+
   return (
     <div className="aboutDiv">
       <Navigation/>
 
       <div className="aboutContainer">
-        <h1 className="intro">Hello<br/>I'm Dave</h1>
-        <p className="aboutContent fade-in">Mountain Biker, Sender, Coder, Peopler?...are just a few words that represent me.  Hello again and nice to meet you.  I am a people person who spent the last 17 years in law enforcement. I take that same passion for helping others out on the streets and now code to help people virtually.  Outside of developing I enjoy being on an adventure with my wife, two boys, and two dogs (Bailey & Sage)  Check out some of my work below and don't hesitate to say hi.</p>
+        <h1 ref={introRef} className={`intro ${introIsVisible ? 'introIsVisible' : 'introIsNotVisible'}`}>Hello<br/>I'm Dave</h1>
+        <p ref={aboutRef} className={`aboutContent ${aboutIsVisible ? 'aboutIsVisible' : 'aboutIsNotVisible'}`}>Mountain Biker, Sender, Coder, Peopler?...are just a few words that represent me.  Hello again and nice to meet you.  I am a people person who spent the last 17 years in law enforcement. I take that same passion for helping others out on the streets and now code to help people virtually.  Outside of developing I enjoy being on an adventure with my wife, two boys, and two dogs (Bailey & Sage)  Check out some of my work below.</p>
       </div>
     </div>
   )
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,20 +1,56 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './Projects.css'
 import Laptop from '../../assets/pics/laptop.png'
 import Mobile from '../../assets/pics/smartphone.png'
 
 
 export default function Projects({project, index}) {
+  const [githubIsHovered, setGithubIsHovered] = useState(false)
+  const [siteIsHovered, setSiteIsHovered] = useState(false)
+  
+  function handleMouseHover (linkSelected){
+    if (linkSelected === 'github'){
+      setGithubIsHovered(true)
+    }
+    if(linkSelected === 'site'){
+      setSiteIsHovered(true)
+    } 
+  }
 
-   return (
+  function handleMouseExit (linkSelected){
+    if (linkSelected === 'github'){
+      setGithubIsHovered(false)
+    }
+    if (linkSelected === 'site'){
+      setSiteIsHovered(false)
+    } 
+  }
+
+  return (
     <div className="ProjectsDiv">
-        <div 
-          className="projectGroup">
-          <div 
-            key={index} 
-            className="showMore hoverInfo"
-          >
+        <div className="projectGroup">
+          <div key={index} className="showMore hoverInfo">
             <p id={index} className="projectDescription">{project.miscInfo}</p>
+            <p className="projectTools">{project.toolsUsed}</p>
+            <a  className="githubLink" 
+                onMouseEnter={()=> {handleMouseHover('github')}}
+                onMouseLeave={()=> {handleMouseExit('github')}}
+                href={project.githubSite} 
+                target="_blank" 
+                style={githubIsHovered ? {color: 'yellow', textDecoration: 'underline'} : {color: 'white', textDecoration: 'none'}}
+            >
+              LINK TO GITHUB
+            </a>
+            <br/>
+            <a  className="siteLink" 
+                onMouseEnter={()=>{handleMouseHover('site')}}
+                onMouseLeave={()=>{handleMouseExit('site')}}
+                href={project.projectSite} 
+                target="_blank" 
+                style={siteIsHovered ? {color: 'yellow', textDecoration: 'underline'} : {color: 'white', textDecoration: 'none'}}
+            >
+              LINK TO SITE
+            </a>
           </div> 
 
           <div className="desktopView">
@@ -26,7 +62,6 @@ export default function Projects({project, index}) {
             <img className="mobilePic" src={project.mobilePic}/>
             <img className="mobile" src={Mobile}/>
           </div>
-
         </div>
      </div>
   )
